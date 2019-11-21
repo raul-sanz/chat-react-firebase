b1f6208e1b9b3dc8bd8f328cf1f4670c59978a5d
raul sanz <estraulsanz@gmail.com>
2019-11-21 13:45:50 -0600

    add firebas config

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-4D0es51H2y9EWLFqq7MP5jUMwFj7bXs",
+    authDomain: "chat-app-54e45.firebaseapp.com",
+    databaseURL: "https://chat-app-54e45.firebaseio.com",
+    projectId: "chat-app-54e45",
+    storageBucket: "chat-app-54e45.appspot.com",
+    messagingSenderId: "309267057372",
+    appId: "1:309267057372:web:26172b8a3b007da6de4219"
+};
